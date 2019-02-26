b37d74160d69af3b83b33c5743e0bfa9f74b9891
Villads Valur Korsholm Nielsen <villads@bambuu.dk>
2019-02-26 09:55:43 +0100

    tilføjede et meyer spil, som underopgave.

--- Meyer.cs
+++ Meyer.cs
@@ -1,8 +1,49 @@
 using System;
-
 namespace FoopMiniProject
 {
     class Meyer
     {
+        public Meyer()
+        {
+            FirstDie = new Dice();
+            SecondDie = new Dice();
+        }
+        public Dice FirstDie { get; }
+        public Dice SecondDie { get; }
+        public bool HasMeyer
+        {
+            get
+            {
+                return FirstDie.Current + SecondDie.Current == 3;
+            }
+        }
+        public void Roll()
+        {
+            FirstDie.Roll();
+            SecondDie.Roll();
+        }
+    }
+
+    class MeyerDemo
+    {
+        public static float LikelyhoodMeyer(float rounds = 10000)
+        {
+            var m = new Meyer();
+            float hasMeyer = 0;
+            float noMeyer = 0;
+
+            for (var i = 0; i < rounds; i++)
+            {
+                m.Roll();
+                if (m.HasMeyer) hasMeyer++;
+                else noMeyer++;
+            }
+            return hasMeyer / rounds * 100;
+        }
+        static void Main(String[] args)
+        {
+            System.Console.WriteLine("Meyer:");
+            System.Console.WriteLine(LikelyhoodMeyer() + "%");
+        }
     }
 }
