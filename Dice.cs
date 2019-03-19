6b46c002d24eade7adb5f20f71b0b9ca4823d99a
Villads V K Nielsen <villadsvalur@gmail.com>
2019-03-19 08:53:13 +0100

    Feature/add yatzy class (#1)
    * added a yatzy class
    
    * nu virker klassen, med en pæn print af de kastede terninger.

--- Dice.cs
+++ Dice.cs
@@ -5,18 +5,18 @@ namespace FoopMiniProject
     public class Dice
     {
         private Random random;
-        public int Current { get; private set; }
-        public Dice()
+        public int numberOfSides { get; private set; }
+        public int CurrentEyes { get; private set; }
+        public Dice(int sides)
         {
             //add seed as to get an actual random value.
             random = new Random(Guid.NewGuid().GetHashCode());
-            Current = 0;
+            CurrentEyes = 0;
+            numberOfSides = sides;
         }
-
         public int Roll()
         {
-            Current = random.Next(1, 7);
-            return Current;
+            return CurrentEyes = random.Next(1, numberOfSides + 1);
         }
     }
 }
---
+++ Yatzy.cs
@@ -0,0 +1,46 @@
+using System;
+namespace FoopMiniProject
+{
+    class Yatzy
+    {
+        public int numberOfDices = 6;
+        public int[] rolledDices = new int[7];
+        public void Roll()
+        {
+            for (var i = 0; i < numberOfDices + 1; i++)
+            {
+                var newDice = new Dice(6);
+                newDice.Roll();
+                rolledDices[i] = newDice.CurrentEyes;
+            }
+        }
+        public int Chance()
+        {
+            int chanceRes = 0;
+            for(var i = 0; 0 < rolledDices.Length; i++)
+            {
+                chanceRes += rolledDices[i];
+            }
+            return chanceRes;
+        }
+
+        public int NumberOf(int eyes)
+        {
+            int numberOfDices = 0;
+            foreach(int diceValue in rolledDices)
+            {
+                numberOfDices++;
+            }
+            return numberOfDices;
+        }
+    }
+    class TestYatzy
+    {
+        static void Main(string[] args)
+        {
+            Yatzy yatzyGame = new Yatzy();
+            yatzyGame.Roll();
+             Console.WriteLine("[{0}]", string.Join(", ", yatzyGame.rolledDices));
+        }
+    }
+}
